1b83707a7c5d186f7fd2fd95977fe898c3d7f79e
parth.p@cearsinfotech.com <parth@2592>
2020-10-21 17:31:46 +0530

    User Migration changes

--- database/migrations/2020_10_12_044432_create_user_details_table.php
+++ database/migrations/2020_10_12_044432_create_user_details_table.php
@@ -16,10 +16,12 @@ class CreateUserDetailsTable extends Migration
         Schema::create('user_details', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->bigInteger('user_id')->unsigned();
+            $table->string('clinic_hospital_name')->nullable();
             $table->text('about_us')->nullable();
             $table->string('country')->nullable();
             $table->string('city')->nullable();
             $table->string('address')->nullable();
+            $table->string('pincode')->nullable();
             $table->string('emergency_contact')->nullable();
             $table->date('dob')->nullable();
             $table->integer('marital_status')->signed()->nullable()->comment('0-Single, 1-Married');
@@ -34,6 +36,7 @@ class CreateUserDetailsTable extends Migration
             $table->string('normal_fees')->nullable();
             $table->string('urgent_fees')->nullable();
             $table->string('home_visit_fees')->nullable();
+            $table->string('delivery_charge')->nullable();
             $table->boolean('urgent')->default(0)->comment('0-Not Urgent Available, 1-Urgent Available');
             $table->boolean('availability')->default(0)->comment('0-Availability, 1-Not Available');
             $table->string('registration_no')->nullable();
@@ -41,8 +44,13 @@ class CreateUserDetailsTable extends Migration
             $table->integer('registration_year')->signed()->nullable();
             $table->string('clinic_name')->nullable();
             $table->string('clinic_city')->nullable();
-            $table->string('clinic_locality')->nullable();
+            $table->string('clinic_locality')->nullable();            
             $table->string('total_experiance_year')->nullable();
+            $table->text('qualification_certificate')->nullable();
+            $table->text('practicing_licence')->nullable();
+            $table->text('health_facility_certificate')->nullable();
+            $table->text('regstration_certificate')->nullable();
+            $table->text('pharmacist_certificate')->nullable();
             $table->boolean('same_timing')->default(0)->comment('0-Same Time Not Available, 1-Same Time Available');
             $table->timestamps();
 
