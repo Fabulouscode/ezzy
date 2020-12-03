f359264ab81e695a038957583c263bfc0130be9b
parth.p@cearsinfotech.com <parth@2592>
2020-12-03 17:13:12 +0530

    Dashbord Api changes

--- app/Http/Controllers/Api/DashboardController.php
+++ app/Http/Controllers/Api/DashboardController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Repositories\UserRepository;
 use App\Repositories\CategoryRepository;
 use App\Repositories\AppointmentRepository;
+use App\Repositories\OrderRepository;
 use App\Repositories\UserTransactionRepository;
 use App\Repositories\NotificationRepository;
 use App\Http\Resources\Api\HeathCareProviderResource;
@@ -16,12 +17,13 @@ use App\Http\Resources\Api\PatientResource;
 
 class DashboardController extends BaseApiController
 {
-    private $user_repo, $category_repo, $appointment_repo, $user_trans_repo, $notification_repo;
+    private $user_repo, $order_repo, $category_repo, $appointment_repo, $user_trans_repo, $notification_repo;
 
     public function __construct(
         UserRepository $user_repo,
         CategoryRepository $category_repo,
         AppointmentRepository $appointment_repo,
+        OrderRepository $order_repo,
         UserTransactionRepository $user_trans_repo,
         NotificationRepository $notification_repo
         )
@@ -30,6 +32,7 @@ class DashboardController extends BaseApiController
         $this->user_repo = $user_repo;
         $this->category_repo = $category_repo;
         $this->appointment_repo = $appointment_repo;
+        $this->order_repo = $order_repo;
         $this->user_trans_repo = $user_trans_repo;
         $this->notification_repo = $notification_repo;
     }
@@ -38,13 +41,15 @@ class DashboardController extends BaseApiController
     {
         $data = array();        
         $data['user'] = $this->user_repo->getbyId($request->user()->id);
-        $data['appointment'] = $this->appointment_repo->getUpcomingAppointment($request);
         if(!empty($request->user()->category_id)){
             if($request->user()->categoryParent->parent_id == '2'){
+                $data['orders'] = $this->order_repo->getActiveOrder($request);
                 $data['user'] = new PharmacyResource($data['user']);
             }else if($request->user()->categoryParent->parent_id == '3'){
+                $data['appointments'] = $this->appointment_repo->getUpcomingAppointment($request);
                 $data['user'] = new LaboratoriesResource($data['user']);
             }else{
+                $data['appointments'] = $this->appointment_repo->getUpcomingAppointment($request);
                 $data['user'] = new HeathCareProviderResource($data['user']);
             }
         }else{
