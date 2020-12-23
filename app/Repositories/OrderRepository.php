d1c36ca0e9d5de4672cbea1e82a4d6942fddd075
parth.p@cearsinfotech.com <parth@2592>
2020-12-23 10:21:00 +0530

    Order History

--- app/Repositories/OrderRepository.php
+++ app/Repositories/OrderRepository.php
@@ -65,6 +65,7 @@ class OrderRepository extends Repository
         
         if(isset($request->delivery_type)){
             $query = $query->where('delivery_type',$request->delivery_type);
+            $query = $query->where('status', '!=','2');
         }
       
         if(!empty($request->user()->category_id)){
