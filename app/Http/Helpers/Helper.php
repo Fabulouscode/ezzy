003d84ac66c67dedcd4ecd0a8a163bd84b303185
parth.p@cearsinfotech.com <parth@2592>
2021-01-12 18:02:09 +0530

    notification msg type

--- app/Http/Helpers/Helper.php
+++ app/Http/Helpers/Helper.php
@@ -74,7 +74,7 @@ class Helper
             'sender_id' => $notification->sender_id,
             'sender_name' => (!empty($sender))?$sender->user_name:'-',
             'receiver_id' => $notification->receiver_id,
-            'type' => $notification->type,
+            'type' => $notification->msg_type,
             'sender_avatar' => (!empty($sender))?$sender->profile_image:'',
             'attachment' => '',
             'notification_count' => $unreadNotification,
@@ -83,7 +83,7 @@ class Helper
 
         $dataTemp = [
             'click_action' => "FLUTTER_NOTIFICATION_CLICK",
-            'screen' => $notification->type,
+            'screen' => $notification->msg_type,
             'object' => $message
         ];
         
@@ -96,32 +96,7 @@ class Helper
                 'body'=>$notification->message
             )
         );
-        
-        // if($receiver->device_type == '1' && $receiver->device_token != '') {
-        //     $msg = array ('title' => config('app.name'), 'body' => $notification->message);
-        //     $message = array(
-        //         "message" => $notification->message,
-        //         "data" => $message,
-        //     );
-        //     $data['registration_ids'] = array($receiver->device_token);
-        //     $data['data'] = $message;
-        //     $data['notification']['sound'] = "default";
-        //     $data['notification']['title'] = config('app.APP_NAME');
-        //     $data['notification']['mutable_content'] = true;
-        //     $data['notification']['category'] = "CustomSamplePush";
-        //     $data['notification']['body'] = $notification->message;
-        //     $data['notification']['badge'] = $unreadNotification;
-        
-        // }
-
-        // if($receiver->device_type == '0' && $receiver->device_token != '') {
-        //     $data = array(
-        //         'to' => $receiver->device_token,
-        //         'data' => $dataTemp,
-        //         'priority'=>'high'
-        //     );
-        // }
-
+   
         if(!empty($data)){
              self::sendCurlRequest($url, $data);
         }
