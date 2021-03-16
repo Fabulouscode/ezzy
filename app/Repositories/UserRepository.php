31dcf1cfc8649a8fd8b05da760943f35bc618373
parth.p@cearsinfotech.com <parth@2592>
2021-03-16 14:40:36 +0530

    urgent appointment booking

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -728,12 +728,12 @@ class UserRepository extends Repository
 
             $query = $query->where('category_id', '4');
 
-            $query = $query->addSelect(DB::raw('((ACOS(SIN('.$request->latitude.' * PI() / 180) * SIN(`users`.`latitude` * PI() / 180) + COS('.$request->latitude.' * PI() / 180) * COS(`users`.`latitude` * PI() / 180) * COS(('.$request->longitude.' - `users`.`longitude`) * PI() / 180)) * 180 / PI()) * 60 * 1.1515 * 1.609344) as distance '))
+            $query = $query->addSelect(DB::raw('((ACOS(SIN('.$request->latitude.' * PI() / 180) * SIN(`users`.`current_latitude` * PI() / 180) + COS('.$request->latitude.' * PI() / 180) * COS(`users`.`current_latitude` * PI() / 180) * COS(('.$request->longitude.' - `users`.`current_longitude`) * PI() / 180)) * 180 / PI()) * 60 * 1.1515 * 1.609344) as distance '))
                            ->where([
-                                    ['users.latitude', '!=', ''],
-                                    ['users.longitude', '!=', '']
+                                    ['users.current_latitude', '!=', ''],
+                                    ['users.current_longitude', '!=', '']
                                 ])
-                           ->havingRaw('distance <= 100')
+                           ->havingRaw('distance <= 200')
                            ->orderBy('distance','asc');
         } else{
 
@@ -747,12 +747,12 @@ class UserRepository extends Repository
 
             if(!empty($request->user()->latitude) && !empty($request->user()->longitude)){
 
-                $query = $query->addSelect(DB::raw('((ACOS(SIN('.$request->user()->latitude.' * PI() / 180) * SIN(`users`.`latitude` * PI() / 180) + COS('.$request->user()->latitude.' * PI() / 180) * COS(`users`.`latitude` * PI() / 180) * COS(('.$request->user()->longitude.' - `users`.`longitude`) * PI() / 180)) * 180 / PI()) * 60 * 1.1515 * 1.609344) as distance '))
+                $query = $query->addSelect(DB::raw('((ACOS(SIN('.$request->user()->latitude.' * PI() / 180) * SIN(`users`.`current_latitude` * PI() / 180) + COS('.$request->user()->latitude.' * PI() / 180) * COS(`users`.`current_latitude` * PI() / 180) * COS(('.$request->user()->longitude.' - `users`.`current_longitude`) * PI() / 180)) * 180 / PI()) * 60 * 1.1515 * 1.609344) as distance '))
                             ->where([
-                                        ['users.latitude', '!=', ''],
-                                        ['users.longitude', '!=', '']
+                                        ['users.current_latitude', '!=', ''],
+                                        ['users.current_longitude', '!=', '']
                                     ])
-                            ->havingRaw('distance <= 100');
+                            ->havingRaw('distance <= 200');
 
             }else{
                 $query = $query->orderBy('id','desc');
