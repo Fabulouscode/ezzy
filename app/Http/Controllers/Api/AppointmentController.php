beb4ff5ebce668482d8ab4e8157b0799c9b5a0ee
Parth.P <parth.p@cearsinfotech.com>
2021-05-31 17:51:09 +0530

    voucher amount change

--- app/Http/Controllers/Api/AppointmentController.php
+++ app/Http/Controllers/Api/AppointmentController.php
@@ -1006,9 +1006,21 @@ class AppointmentController extends BaseApiController
                     }  
                 }
             }
-     
+
+            $voucher_amount_apply = 0;
             if(!empty($appointment_details->voucher_code_id)){
-                $transaction_amount = $transaction_amount - $appointment_details->voucher_amount;
+                $voucher_code = $this->voucher_code_repo->getbyIdVoucherType($appointment_details->voucher_code_id, '1'); 
+                if(!empty($voucher_code) && !empty($voucher_code->id)){
+                    if(!empty($voucher_code->percentage)){
+                        $voucher_amount_apply = (($transaction_amount / 100 ) * $voucher_code->percentage);
+                    }
+                    if($voucher_code->fix_amount > $voucher_amount_apply){
+                        $voucher_amount_apply = $voucher_amount_apply;
+                    }else {
+                        $voucher_amount_apply = $voucher_code->fix_amount;
+                    }
+                }
+                $transaction_amount = $transaction_amount - $voucher_amount_apply;
             }
 
             $update = [
