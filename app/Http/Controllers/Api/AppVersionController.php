528667edeac4d186714273b11ef726255ca3a77c
Parth <parth@gmail.com>
2021-10-30 16:18:55 +0530

    app version set

--- app/Http/Controllers/Api/AppVersionController.php
+++ app/Http/Controllers/Api/AppVersionController.php
@@ -12,7 +12,7 @@ class AppVersionController extends BaseApiController
      
     public function checkAndroidVersion($version)
     {
-        $data = AppVersion::where('android_version',$version)->where('id','1')->first();
+        $data = AppVersion::where('android_version','<=' ,$version)->where('id','1')->first();
         if(!empty($data)){
             return self::sendSuccess(true,'App Version Latest');
         }
@@ -21,7 +21,7 @@ class AppVersionController extends BaseApiController
    
     public function checkIOSVersion($version)
     {
-        $data = AppVersion::where('ios_version',$version)->where('id','1')->first();
+        $data = AppVersion::where('ios_version' ,'<=' ,$version)->where('id','1')->first();
         if(!empty($data)){
             return self::sendSuccess(true,'App Version Latest');
         }
