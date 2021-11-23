77120d81bb60684679ef7abca7bbe4449ab48b5a
parth@pipaliyan@gmail.com <parth.p@cearsinfotech.com>
2021-11-23 14:17:56 +0530

    Update User_transaction.php
    user transaction chnages

--- app/Models/User_transaction.php
+++ app/Models/User_transaction.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User_transaction extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     
     public $status_value = array(
         '0' => 'Success',
