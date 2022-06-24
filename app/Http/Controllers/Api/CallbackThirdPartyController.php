17e6063132016b41bab18d9072b5aeb99238756d
parth <72193184+parth-cearsinfotech@users.noreply.github.com>
2022-06-24 14:37:09 +0530

    InterswitchCallback add

--- app/Http/Controllers/Api/CallbackThirdPartyController.php
+++ app/Http/Controllers/Api/CallbackThirdPartyController.php
@@ -69,40 +69,39 @@ class CallbackThirdPartyController extends BaseApiController
     {
         \Log::info("============Callback getInterswitchCallback=================");
         \Log::info(json_encode($request->all()));
-  
-        // if(!empty($request->event) && $request->event == "TRANSACTION.COMPLETED"){
-        //     if(!empty($request->data) && !empty($request->data['reference']) && !empty($request->data['gateway_response']) && !empty($request->data['customer']) && !empty($request->data['customer']['email']) && $request->data['gateway_response'] == 'Successful'){
-        //         $walletBalance = $this->user_transaction_repo->getPendingTransactionCallback($request->data['customer']['email'], $request->data['reference']); 
-        //         if(!empty($walletBalance) && !empty($walletBalance->id)){
-        //             $wallet_transaction = [
-        //                 'user_id'=> $walletBalance->user_id,
-        //                 'transaction_date'=> $this->user_transaction_repo->getCurrentDateTime(),
-        //                 'amount'=> ($request->data['amount'] / 100),                        
-        //                 'payment_gateway_response'=> json_encode($request->all()),
-        //                 'payment_gateway_full_response'=> json_encode($request->all()),
-        //                 'status'=> '0',
-        //                 'transaction_msg'=>'Add Wallet to online pay',
-        //             ];  
-        //             $this->user_transaction_repo->dataCrud($wallet_transaction, $walletBalance->id);
-        //             $add_transaction = [
-        //                 'user_id'=> $walletBalance->user_id,
-        //                 'transaction_date'=> $this->user_transaction_repo->getCurrentDateTime(),
-        //                 'amount'=> ($request->data['amount'] / 100),                             
-        //                 'payment_gateway_response'=> json_encode($request->all()),
-        //                 'payment_gateway_full_response'=> json_encode($request->all()),
-        //                 'mode_of_payment'=> '0',
-        //                 'transaction_type'=> '0',
-        //                 'wallet_transaction'=> '1',
-        //                 'payout_status'=> '0',
-        //                 'status'=> '0',
-        //                 'transaction_msg'=>'Wallet Topup',
-        //             ];
-        //             $this->user_transaction_repo->dataCrud($add_transaction);
-        //             $this->user_repo->userWalletUpdate($walletBalance->user_id); 
-        //         }
+        if(!empty($request->event) && $request->event == "TRANSACTION.COMPLETED"){
+            if(!empty($request->data) && !empty($request->uuid) && !empty($request->data) && !empty($request->data['merchantCustomerId']) && !empty($request->data['amount']) && !empty($request->data['merchantReference']) && isset($request->data['responseCode']) && $request->data['responseCode'] == '00'){
+                $walletBalance = $this->user_transaction_repo->getPendingTransaction($request->data['merchantCustomerId'], $request->uuid);  
+                if(!empty($walletBalance) && !empty($walletBalance->id)){
+                    $wallet_transaction = [
+                        'user_id'=> $walletBalance->user_id,
+                        'transaction_date'=> $this->user_transaction_repo->getCurrentDateTime(),
+                        'amount'=> ($request->data['amount'] / 100),                        
+                        'payment_gateway_response'=> $request->uuid,
+                        'payment_gateway_full_response'=> json_encode($request->all()),
+                        'status'=> '0',
+                        'transaction_msg'=>'Add Wallet to online pay',
+                    ];  
+                    $this->user_transaction_repo->dataCrud($wallet_transaction, $walletBalance->id);
+                    $add_transaction = [
+                        'user_id'=> $walletBalance->user_id,
+                        'transaction_date'=> $this->user_transaction_repo->getCurrentDateTime(),
+                        'amount'=> ($request->data['amount'] / 100),                             
+                        'payment_gateway_response'=> $request->uuid,
+                        'payment_gateway_full_response'=> json_encode($request->all()),
+                        'mode_of_payment'=> '0',
+                        'transaction_type'=> '0',
+                        'wallet_transaction'=> '1',
+                        'payout_status'=> '0',
+                        'status'=> '0',
+                        'transaction_msg'=>'Wallet Topup',
+                    ];
+                    $this->user_transaction_repo->dataCrud($add_transaction);
+                    $this->user_repo->userWalletUpdate($walletBalance->user_id); 
+                }
                  
-        //     }
-        // }
+            }
+        }
 
     }
 
