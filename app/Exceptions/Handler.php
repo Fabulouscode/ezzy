e2bd889511aea088e9be56228420ca7b21adb85b
parth-elaunch <parth.p@elaunchinfotech.in>
2023-05-10 10:41:22 +0530

    exception handling

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,9 +2,18 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Auth\AuthenticationException;
-use Auth; 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Exceptions\ThrottleRequestsException;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Illuminate\Support\Facades\Log;
+use Throwable;
+
 
 class Handler extends ExceptionHandler
 {
@@ -14,7 +23,8 @@ class Handler extends ExceptionHandler
      * @var array
      */
     protected $dontReport = [
-        //
+        // oath exception handling BearerTokenValidator
+        "\League\OAuth2\Server\Exception\OAuthServerException",
     ];
 
     /**
@@ -34,7 +44,86 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        //
+        $this->reportable(function (Throwable $e) {
+            //
+        });
+    }
+
+    public function render($request, Throwable  $exception)
+    {
+        if ($request->expectsJson()) {
+            if ($exception instanceof AccessDeniedHttpException || $exception instanceof AuthorizationException) {
+                Log::info('start AccessDeniedHttpException, AuthorizationException');
+                $data = array(
+                    'code' => $exception->getCode(),
+                    'file' => $exception->getFile(),
+                    'line' => $exception->getLine(),
+                    'message' => $exception->getMessage(),
+                    'trace' => $exception->getTraceAsString(),
+                );
+                Log::info(json_encode($data));
+                Log::info('stop AccessDeniedHttpException, AuthorizationException');
+                return response()->json([
+                    'success' => false,
+                    'error' => config('app.debug') ? $exception->getMessage().' at '.$exception->getLine() : 'Some Internal Server Error',
+                    'message' => 'Some Internal Server Error'
+                ], 500);
+            }
+
+            if ($exception instanceof MethodNotAllowedHttpException || $exception instanceof ModelNotFoundException || $exception instanceof NotFoundHttpException) {
+                Log::info('start MethodNotAllowedHttpException, ModelNotFoundException, NotFoundHttpException');
+                $data = array(
+                    'code' => $exception->getCode(),
+                    'file' => $exception->getFile(),
+                    'line' => $exception->getLine(),
+                    'message' => $exception->getMessage(),
+                    'trace' => $exception->getTraceAsString(),
+                );
+                Log::info(json_encode($data));
+                Log::info('stop MethodNotAllowedHttpException, ModelNotFoundException, NotFoundHttpException');
+                return response()->json([
+                    'success' => false,
+                    'error' => config('app.debug') ? $exception->getMessage().' at '.$exception->getLine() : 'Some Internal Server Error',
+                    'message' => 'Some Internal Server Error'
+                ], 500);
+            }
+            if ($exception instanceof ThrottleRequestsException) {
+                Log::info('start ThrottleRequestsException');
+                $data = array(
+                    'code' => $exception->getCode(),
+                    'file' => $exception->getFile(),
+                    'line' => $exception->getLine(),
+                    'message' => $exception->getMessage(),
+                    'trace' => $exception->getTraceAsString(),
+                );
+                Log::info(json_encode($data));
+                Log::info('stop ThrottleRequestsException');
+                return response()->json([
+                    'success' => false,
+                    'error' => config('app.debug') ? $exception->getMessage().' at '.$exception->getLine() : 'Some Internal Server Error',
+                    'message' => 'Some Internal Server Error'
+                ], 500);
+            }
+        }
+        if ($exception instanceof \League\OAuth2\Server\Exception\OAuthServerException && $exception->getCode() == 9) {
+            Log::info('start OAuthServerException');
+            $data = array(
+                'code' => $exception->getCode(),
+                'file' => $exception->getFile(),
+                'line' => $exception->getLine(),
+                'message' => $exception->getMessage(),
+                'trace' => $exception->getTraceAsString(),
+            );
+            Log::info(json_encode($data));
+            Log::info('stop OAuthServerException');
+            return response()->json([
+                'success' => false,
+                'error' => config('app.debug') ? $exception->getMessage().' at '.$exception->getLine() : 'Some Internal Server Error',
+                'message' => 'Some Internal Server Error'
+            ], 500);
+        }
+
+        return parent::render($request, $exception);
     }
 
     protected function unauthenticated($request, AuthenticationException $exception)
