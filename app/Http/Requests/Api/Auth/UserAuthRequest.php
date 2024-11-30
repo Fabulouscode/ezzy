9d189b7f5c90f718b07c6da6ab583fe37c99d1c3
parth-elaunch <parth.p@elaunchinfotech.in>
2024-11-30 11:00:29 +0530

    unique check

--- app/Http/Requests/Api/Auth/UserAuthRequest.php
+++ app/Http/Requests/Api/Auth/UserAuthRequest.php
@@ -27,7 +27,7 @@ class UserAuthRequest extends FormRequest
     {
         return [
            'register_type' => 'required|in:1,2',
-           'email' => 'required|string|email|max:255|unique:users,email',
+           'email' => 'required|string|email|max:255|unique:users,email,NOTNULL,password,deleted_at,NULL',
            'mobile_no' => 'required|numeric|starts_with: 1,1,2,3,4,5,6,7,8,9|unique:users,mobile_no,NOTNULL,password,deleted_at,NULL,country_code,'.$this->country_code,
            'country_code' => 'required',
            'password' => 'required|string|min:8',
