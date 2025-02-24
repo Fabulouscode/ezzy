b82465a6a4725e39efb024e0a64fad2a3e495bbd
parth-elaunch <parth.p@elaunchinfotech.in>
2025-02-24 17:57:27 +0530

    appoinment changes

--- app/Http/Controllers/Api/AppointmentController.php
+++ app/Http/Controllers/Api/AppointmentController.php
@@ -403,7 +403,7 @@ class AppointmentController extends BaseApiController
             \Log::info("Provider is busy ".json_encode($check_appointment));   
             return self::sendError([], 'Please change the appointment time. The provider is busy.');
         }
-        
+
         //urgent appiointment any start check
         $check_appointment = $this->appointment_repo->checkUserUrgentAppointmemt($request);
         if(!empty($check_appointment)){
@@ -1750,7 +1750,7 @@ class AppointmentController extends BaseApiController
                                         'receiver_id' => $appointmentRequest->client_id,
                                         'title' => 'Urgent Appointment',
                                         'message' => 'Urgent appointment request accepted by '.$doctorUser->user_name,
-                                        'parameter' => json_encode(['appointment_id'=> $appointmentRequest->id, 'status'=>$appointmentRequest->status,'notification_time'=>Carbon::now()->format('Y-m-d H:i:s')]),
+                                        'parameter' => json_encode(['appointment_id'=> $appointmentRequest->id, 'status'=>$appointmentRequest->status,'notification_time'=>Carbon::now()->format('Y-m-d H:i:s'), 'urgent' => $appointment_details->urgent]),
                                         'msg_type' => '2',
                                     ];  
                 try{
@@ -1764,7 +1764,7 @@ class AppointmentController extends BaseApiController
                                         'receiver_id' => $request->user_id,
                                         'title' => 'Urgent Appointment',
                                         'message' => 'Urgent appointment request accepted',
-                                        'parameter' => json_encode(['appointment_id'=> $appointmentRequest->id, 'status'=>$appointmentRequest->status,'notification_time'=>Carbon::now()->format('Y-m-d H:i:s')]),
+                                        'parameter' => json_encode(['appointment_id'=> $appointmentRequest->id, 'status'=>$appointmentRequest->status,'notification_time'=>Carbon::now()->format('Y-m-d H:i:s'), 'urgent' => $appointment_details->urgent]),
                                         'msg_type' => '2',
                                     ];  
                 try{
